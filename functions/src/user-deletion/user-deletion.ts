91a44b2cb8fb75fe7cea5b6eee3c42bd52a9442f
Mark Goho <markgoho@gmail.com>
2025-10-12 14:57:31 -0400

    Refactor user deletion logging to use Firebase logger
    - Replaced console logging with Firebase logger for better integration and consistency in the handleUserDeleted function.
    - Updated error handling to utilize logger for error messages, improving error tracking and reporting.

--- functions/src/user-deletion/user-deletion.ts
+++ functions/src/user-deletion/user-deletion.ts
@@ -1,4 +1,5 @@
 import { getFirestore } from "firebase-admin/firestore";
+import { logger } from "firebase-functions";
 import { type auth } from "firebase-functions/v1";
 
 export async function handleUserDeleted(user: auth.UserRecord) {
@@ -9,9 +10,9 @@ export async function handleUserDeleted(user: auth.UserRecord) {
     // Delete the member document that matches the auth UID
     await firestore.collection("members").doc(uid).delete();
 
-    console.log(`Deleted member document for user: ${user.uid}`);
+    logger.log(`Deleted member document for user: ${user.uid}`);
   } catch (error: unknown) {
-    console.error("Error deleting member document:", error);
+    logger.error("Error deleting member document:", error);
     throw error;
   }
 }
